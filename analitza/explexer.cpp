0ba5fbb4f20e5391ac4c1e8b1f58ec09e293084f
Aleix Pol Gonzalez <aleixpol@gmail.com>
2008-09-19 18:02:27 +0000

    Some code polishing on the lexer.
    svn path=/trunk/KDE/kdeedu/kalgebra/; revision=862735

--- analitza/explexer.cpp
+++ analitza/explexer.cpp
@@ -83,9 +83,15 @@ void ExpLexer::getToken(const QString &a, int &pos)
 	int oldpos=pos;
 	TOKEN ret(-1, pos);
 	
-	if(pos==a.length()) {
+	if(pos>=a.length()) {
 		ret.type = ExpressionTable::EOF_SYMBOL;
-	} else if(a[pos].decompositionTag()==QChar::Super) {
+	} else if(a.length()>pos+1 && m_longOperators.contains(QString(a[pos])+a[pos+1])) {
+		ret.type=m_longOperators[QString(a[pos])+a[pos+1]];
+		pos+=2;
+	} else if(m_operators.contains(a[pos])) {
+		ret.type=m_operators[a[pos]];
+		pos++;
+    } else if(a[pos].decompositionTag()==QChar::Super) {
 		QString super;
 		for(int i=pos; i<a.count() && a[i].decompositionTag()==QChar::Super; i++) {
 			ret.type = ExpressionTable::tPow;
@@ -95,19 +101,17 @@ void ExpLexer::getToken(const QString &a, int &pos)
 		}
 		m_tokens.append(TOKEN(ExpressionTable::tPow, pos, QString(), 0));
 		ret=TOKEN(ExpressionTable::tVal, oldpos, "<cn>"+super+"</cn>", pos-oldpos);
-	} else if(a[pos].isDigit() || (a[pos]=='.' && a[1].isDigit())) {
+	} else if(a[pos].isDigit() || a[pos]=='.') {
 		int coma=0;
-		
-		int i;
-		for(i=pos; i<a.length() && (a[i].isDigit() || (i+1<a.length() && a[i]=='.' && a[i+1]!='.')); i++){
-			if(a[i]=='.')
+		for(; pos<a.length() && (a[pos].isDigit() || (pos+1<a.length() && a[pos]=='.' && a[pos+1]!='.')); pos++){
+			if(a[pos]=='.')
 				coma++;
-			ret.val += a[i];
-			pos++;
+			ret.val += a[pos];
 		}
-		for(; i<a.length() && a[i].isSpace(); i++) {}
 		
-		if(i<a.length() && (a[i] == '(' || a[i].isLetter()))
+		for(; pos<a.length() && a[pos].isSpace(); pos++) {}
+		
+		if(pos<a.length() && (a[pos] == '(' || a[pos].isLetter()))
 			m_tokens.append(TOKEN(ExpressionTable::tMul, 0));
 		
 		QStringList attrib;
@@ -122,28 +126,20 @@ void ExpLexer::getToken(const QString &a, int &pos)
 			ret.val = QString("<cn %1>%2</cn>").arg(attrib.join(" ")).arg(ret.val);
 		ret.type= ExpressionTable::tVal;
 	} else if(a[pos].isLetter()) {//es una variable o funcret.val += a[0];
-		int i;
-		for(i=pos; i<a.length() && a[i].isLetterOrNumber() && a[i].decompositionTag()==QChar::NoDecomposition; i++){
-			ret.val += a[i];
-			pos++;
+		for(; pos<a.length() && a[pos].isLetterOrNumber() && a[pos].decompositionTag()==QChar::NoDecomposition; pos++){
+			ret.val += a[pos];
 		}
 		
-		for(; i<a.length() && a[i].isSpace(); i++) {}
+		for(; pos<a.length() && a[pos].isSpace(); pos++) {}
 		
-		if(i<a.length() && (a[i]=='(' || a[i].isLetterOrNumber()) && a[i].decompositionTag()==QChar::NoDecomposition) {
+		if(pos<a.length() && (a[pos]=='(' || a[pos].isLetterOrNumber()) && a[pos].decompositionTag()==QChar::NoDecomposition) {
 			ret.type=ExpressionTable::tFunc;
-		} else if(i<a.length() && a[i]=='{' && a[i].decompositionTag()==QChar::NoDecomposition) {
+		} else if(pos<a.length() && a[pos]=='{' && a[pos].decompositionTag()==QChar::NoDecomposition) {
 			ret.type=ExpressionTable::tBlock;
 		} else {
 			ret.val = QString("<ci>%1</ci>").arg(ret.val);
 			ret.type= ExpressionTable::tVal;
 		}
-	} else if(a.length()>pos+1 && m_longOperators.contains(QString(a[pos])+a[pos+1])) {
-		ret.type=m_longOperators[QString(a[pos])+a[pos+1]];
-		pos+=2;
-	} else if(a.length()>pos && m_operators.contains(a[pos])) {
-		ret.type=m_operators[a[pos]];
-		pos++;
 	} else {
 		ret.val=-1;
 		m_err=i18n("Unknown token %1", a[pos]);
