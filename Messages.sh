0fb7f1f08982e413b646346aa1a220fe9fb778e5
Aleix Pol Gonzalez <aleixpol@gmail.com>
2008-10-18 14:59:05 +0000

    translation directory
    svn path=/trunk/KDE/kdeedu/kalgebra/; revision=873023

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$XGETTEXT kalgebra/*.cpp analitza/*.cpp -o $podir/kalgebra.pot
+$XGETTEXT src/*.cpp analitza/*.cpp -o $podir/kalgebra.pot
 $XGETTEXT plasmoid/*.cpp -o $podir/plasma_applet_kalgebra.pot
 
