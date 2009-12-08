5de19acbc52b7339a1fbdfb0ed0f0491a338d1cf
Burkhard Lück <lueck@hube-lueck.de>
2009-12-08 16:31:16 +0000

    add missing subdir for message extraction, 68 new msgs CCMAIL:kde-i18n-doc@kde.org
    svn path=/trunk/KDE/kdeedu/kalgebra/; revision=1060352

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$XGETTEXT src/*.cpp analitza/*.cpp -o $podir/kalgebra.pot
+$XGETTEXT src/*.cpp analitzagui/*.cpp analitza/*.cpp -o $podir/kalgebra.pot
 $XGETTEXT plasmoid/*.cpp -o $podir/plasma_applet_kalgebra.pot
 
