9bdd5a15f0878df23e052ee5d9254fd25aacb0c0
Alexander Rieder <alexanderrieder@gmail.com>
2010-08-29 23:33:55 +0000

    move QtMmlWidget to libkdeedu to make it possible to reuse it in Cantor. CCMAIL: kde-edu@kde.org
    svn path=/trunk/KDE/kdeedu/kalgebra/; revision=1169724

--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -32,7 +32,7 @@
 #include <QFormLayout>
 #include <KLineEdit>
 #include <KLocale>
-#include <qtmmlwidget/qtmmlwidget.h>
+#include <QtMmlWidget>
 
 Dictionary::Dictionary(QWidget *p) : QWidget(p)
 {
@@ -41,9 +41,9 @@ Dictionary::Dictionary(QWidget *p) : QWidget(p)
 	m_sortProxy->setSourceModel(m_ops);
 	m_sortProxy->sort(2, Qt::AscendingOrder);
 	m_sortProxy->setFilterKeyColumn(2);
-	
+
 	m_vars = new Analitza::Variables;
-	
+
 	QGroupBox *descr=new QGroupBox(i18n("Information"), this);
 	descr->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 	QFormLayout *descrLayo=new QFormLayout;
@@ -62,25 +62,25 @@ Dictionary::Dictionary(QWidget *p) : QWidget(p)
 	m_graph->setReadOnly(true);
 	m_graph->setViewport(QRect(QPoint(-30, 7), QPoint(30, -7)));
 	m_graph->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-	
+
 	m_name->setIndent(10);
 	m_descr->setIndent(10);
 	m_sample->setIndent(10);
 	m_example->setIndent(10);
-	
+
 	m_example->setTextInteractionFlags(Qt::TextSelectableByMouse);
 
 	descrLayo->addRow(i18n("<b>%1</b>", m_ops->headerData(0, Qt::Horizontal).toString()), m_name);
 	descrLayo->addRow(i18n("<b>%1</b>", m_ops->headerData(1, Qt::Horizontal).toString()), m_descr);
 	descrLayo->addRow(i18n("<b>%1</b>", m_ops->headerData(2, Qt::Horizontal).toString()), m_sample);
-	descrLayo->addRow(i18n("<b>%1</b>", m_ops->headerData(3, Qt::Horizontal).toString()), m_example); 
+	descrLayo->addRow(i18n("<b>%1</b>", m_ops->headerData(3, Qt::Horizontal).toString()), m_example);
 	descrLayo->addRow(i18n("<b>Formula</b>"), m_formula);
  	descrLayo->setFieldGrowthPolicy(QFormLayout::ExpandingFieldsGrow);
 	graphLayo->addWidget(descr);
 // 	graphLayo->addWidget(m_formula);
 	graphLayo->addWidget(m_graph);
 	descr->setLayout(descrLayo);
-	
+
 	m_funcs->clear();
 	m_formula->setContent("<math />");
 // 	connect(m_list, SIGNAL(clicked ( const QModelIndex & )), this, SLOT(activated(const QModelIndex &)));
@@ -94,7 +94,7 @@ Dictionary::~Dictionary()
 void Dictionary::activated(const QModelIndex& idx, const QModelIndex& prev)
 {
 	Q_UNUSED(prev);
-	
+
 	m_funcs->clear();
 	if(idx.isValid()) {
 		QModelIndex nameIdx, descriptionIdx, sampleIdx, exampleIdx;
@@ -102,24 +102,24 @@ void Dictionary::activated(const QModelIndex& idx, const QModelIndex& prev)
 		descriptionIdx = idx.sibling(idx.row(), 1);
 		sampleIdx = idx.sibling(idx.row(), 2);
 		exampleIdx = idx.sibling(idx.row(), 3);
-		
+
 		QString name=m_sortProxy->data(nameIdx).toString();
 		QString description=m_sortProxy->data(descriptionIdx).toString();
 		QString sample=m_sortProxy->data(sampleIdx).toString();
 		QString example=m_sortProxy->data(exampleIdx).toString();
-		
+
 		Analitza::Expression e(example, false);
-		
+
 		m_name->setText(name);
 		m_descr->setText(description);
 		m_sample->setText(sample);
 		m_example->setText(example);
-		
+
 		QString error;
 		m_formula->setContent(e.toMathMLPresentation(), &error);
 		if(!error.isEmpty())
 			qDebug() << "dict formula error: " << error << e.toMathMLPresentation();
-		
+
 		m_funcs->addFunction(function("func", e, m_vars, QColor(0,150,0), 0, 0));
 	} else {
 		QString error;
