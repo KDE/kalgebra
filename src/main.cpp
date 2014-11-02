d2b66c1a7f69bf9d5cf83f7120277f04ba2a00a4
Montel Laurent <montel@kde.org>
2014-11-02 12:48:46 +0100

    Convert to reverse dns desktop. Allow to translate apps

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,15 @@
 
 #include <QApplication>
 #include <KAboutData>
+#include <KLocalizedString>
 #include "kalgebra.h"
 
 int main(int argc, char *argv[])
 {
+        KLocalizedString::setApplicationDomain("kalgebra");
 	QApplication app(argc, argv);
 	app.setApplicationName("kalgebra");
-	app.setApplicationDisplayName(app.tr("KAlgebra"));
+	app.setApplicationDisplayName(i18n("KAlgebra"));
 	app.setApplicationVersion("0.99");
 	
 	KAlgebra widget;
